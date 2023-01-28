1537e348987fa4c5bd89dab3d2021c3ea09e063b
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-01-28 02:00:09 +0100

    Renamed to index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
-function hamburger() {
-  let hamburgerIcon = document.querySelector("#hamburger-icon");
-  let mainNavShow = document.querySelector(".main-nav");
-  if (hamburgerIcon.getAttribute("src") == "/LuleNorthernLightsCinema/static/hamburger-icon.svg") {
-    hamburgerIcon.setAttribute("src", "/LuleNorthernLightsCinema/static/close-icon.svg");
-    mainNavShow.classList.add("show");
-  } else {
-    hamburgerIcon.setAttribute("src", "/LuleNorthernLightsCinema/static/hamburger-icon.svg");
-    mainNavShow.classList.remove("show");
-  }
-}
+import { hamburger } from './hamburger-menu.js';
+import { apiHandler } from './api-handler.js';
 // adding cards to frontpage
 let movies;
 
@@ -127,4 +118,4 @@ async function displayCards() {
     });
   });
 }
-displayCards();
+// displayCards();
